fcdea804f0baaf147796ce3fd0f57eb764013cd1
donghyuk <udhyk27@gmail.com>
2026-03-31 11:55:59 +0900

    env 설정

--- music-trade-java/src/main/java/com/example/musictrade/MusictradeApplication.java
+++ music-trade-java/src/main/java/com/example/musictrade/MusictradeApplication.java
@@ -1,5 +1,6 @@
 package com.example.musictrade;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MusictradeApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("BESU_RPC_URL", dotenv.get("BESU_RPC_URL"));
+		System.setProperty("BESU_CONTRACT_ADDRESS", dotenv.get("BESU_CONTRACT_ADDRESS"));
+		System.setProperty("BESU_PRIVATE_KEY", dotenv.get("BESU_PRIVATE_KEY"));
+
+
 		SpringApplication.run(MusictradeApplication.class, args);
+
 	}
 
 }
