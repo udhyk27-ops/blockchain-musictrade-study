fe4c0f42c92eb9fd69ea6d549056b829fcf3c0b4
donghyuk <udhyk27@gmail.com>
2026-04-01 15:42:51 +0900

    rpc url application.properties에서 주입받는 형태로

--- music-trade-java/src/main/java/com/example/musictrade/controller/PageController.java
+++ music-trade-java/src/main/java/com/example/musictrade/controller/PageController.java
@@ -1,13 +1,19 @@
 package com.example.musictrade.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
 
+    @Value("${besu.contract.address}")
+    private String contractAddress;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("contractAddress", contractAddress);
         return "index";
     }
 }
